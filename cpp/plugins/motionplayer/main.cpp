a0a0697038288b649944c6c57968cfe484a37173
xiaocongyu66 <xiaocongyu1@qq.com>
2026-05-23 23:05:54 +0000

    fix(motionplayer): run post registration after D3D classes

--- cpp/plugins/motionplayer/main.cpp
+++ cpp/plugins/motionplayer/main.cpp
@@ -570,7 +570,6 @@ static void PreRegistCallback() {}
 static void PostUnregistCallback() {}
 
 NCB_PRE_REGIST_CALLBACK(PreRegistCallback);
-NCB_POST_REGIST_CALLBACK(PostRegistCallback);
 NCB_POST_UNREGIST_CALLBACK(PostUnregistCallback);
 
 // ============================================================
@@ -702,3 +701,5 @@ NCB_REGISTER_CLASS(D3DEmotePlayer) {
     NCB_METHOD(getOuterForce);
     NCB_METHOD_RAW_CALLBACK(contains, &EmotePlayer::containsCompat, 0);
 }
+
+NCB_POST_REGIST_CALLBACK(PostRegistCallback);
