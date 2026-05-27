cf79cd6c41739b07018b8dccdea23ee6c94d4f07
xiaocongyu66 <xiaocongyu1@qq.com>
2026-05-27 09:59:22 +0000

    Add missing size import

--- platforms/android/app/java/org/github/krkr2/LauncherSettingsActivity.kt
+++ platforms/android/app/java/org/github/krkr2/LauncherSettingsActivity.kt
@@ -27,7 +27,7 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
